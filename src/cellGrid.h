b99f06a2447ba26e2c953a16da064a61f448170e
torto <torte.torto@gmail.com>
2025-01-06 22:34:42 +0100

    try to update only cells that change and their surrounding

--- src/cellGrid.h
+++ src/cellGrid.h
@@ -8,7 +8,19 @@
 #include <memory>
 #include "random.h"
 #include <SFML/System/Vector2.hpp>
+#include <unordered_set>
+#include <iostream>
 
+
+namespace std {
+template <> struct hash<std::pair<int, int>> {
+    inline size_t operator()(const std::pair<int, int> &v) const {
+        std::hash<int> int_hasher;
+        return int_hasher(v.first) ^ int_hasher(v.second);
+    }
+};
+
+}
 
 class CellGrid : public sf::Drawable, public sf::Transformable
 {
@@ -40,6 +52,8 @@ public:
 			nextRow.reserve(m_nbCol);
 			for (int j{}; j < m_nbCol; j++)
 			{
+				m_indexToUpdate.insert(std::make_pair(i, j));
+
 				if (random)
 				{
 					row.emplace_back(static_cast<Cell::Type>(Random::get(0, static_cast<int>(Cell::Type::maxType) - 1)));
@@ -49,7 +63,7 @@ public:
 					row.emplace_back(Cell::Type::dead);
 				}
 
-				nextRow.emplace_back(Cell::Type::dead);
+				nextRow.emplace_back(row.back());
 
 				sf::Vertex* triangles = &m_vertices[(i + j * m_nbRow) * 6];
 
@@ -76,15 +90,19 @@ public:
 
 	void prepareNextGrid()
 	{
-		for (int i{}; i < m_nbRow; i++)
+		m_nextIndexToUpdate.reserve(2*m_nextGrid.size());
+		for (const auto& coord : m_indexToUpdate)
 		{
-			for (int j{}; j < m_nbCol; j++)
+			Cell::Type newType = m_cells[m_grid[coord.first][coord.second]].get()->generateNextType(m_grid, coord.first, coord.second);
+			if (newType != m_grid[coord.first][coord.second])
 			{
-				m_nextGrid[i][j] = m_cells[m_grid[i][j]].get()->generateNextType(m_grid, i, j);
+				m_nextGrid[coord.first][coord.second] = newType;
+				insertCoordSquare(m_nextIndexToUpdate, coord);
+				//m_nextIndexToUpdate.insert({ coord.first, coord.second });
 			}
 		}
 	}
-
+	
 	void instantUpdate()
 	{
 		update(m_tBetweenUdpates);
@@ -97,23 +115,24 @@ public:
 		m_tRemaining += m_tBetweenUdpates;
 
 		prepareNextGrid();
-		for (int i{}; i < m_nbRow; i++)
+
+		for (const auto& coord : m_indexToUpdate)
 		{
-			for (int j{}; j < m_nbCol; j++)
-			{
-				m_grid[i][j] = m_nextGrid[i][j];
+			m_grid[coord.first][coord.second] = m_nextGrid[coord.first][coord.second];
 
-				sf::Vertex* triangles = &m_vertices[(i + j * m_nbRow) * 6];
+			sf::Vertex* triangles = &m_vertices[(coord.first + coord.second * m_nbRow) * 6];
 
-				sf::Color cellColor = m_cells[m_grid[i][j]].get()->getColor();
-				triangles[0].color = cellColor;
-				triangles[1].color = cellColor;
-				triangles[2].color = cellColor;
-				triangles[3].color = cellColor;
-				triangles[4].color = cellColor;
-				triangles[5].color = cellColor;
-			}
+			sf::Color cellColor = m_cells[m_grid[coord.first][coord.second]].get()->getColor();
+			triangles[0].color = cellColor;
+			triangles[1].color = cellColor;
+			triangles[2].color = cellColor;
+			triangles[3].color = cellColor;
+			triangles[4].color = cellColor;
+			triangles[5].color = cellColor;
+
 		}
+		m_indexToUpdate = m_nextIndexToUpdate;
+		m_nextIndexToUpdate.clear();
 	}
 
 	void setCell(Cell::Type type, const sf::Vector2i& pos)
@@ -134,6 +153,28 @@ public:
 		triangles[5].color = cellColor;
 	}
 
+	void insertCoordSquare(std::unordered_set<std::pair<int, int>>& set, const std::pair<int, int>& coord)
+	{
+		//std::pair<int, int> test{ 1, 2 };
+		for (int i{ coord.first - 1 }; i <= coord.first + 1; i++)
+		{
+			for (int j{ coord.second - 1 }; j <= coord.second + 1; j++)
+			{
+				set.insert(transformToValidPair(std::make_pair(i, j)));
+				//std::make_pair(i, j);
+			}
+		}
+	}
+
+	std::pair<int, int>& transformToValidPair(std::pair<int, int>& coord)
+	{
+		if (coord.first < 0) coord.first += m_nbRow;
+		if (coord.first >= m_nbRow) coord.first -= m_nbRow;
+		if (coord.second < 0) coord.second += m_nbCol;
+		if (coord.second >= m_nbCol) coord.second -= m_nbCol;
+
+		return coord;
+	}
 	
 private:
 
@@ -149,7 +190,8 @@ private:
 	std::vector<std::unique_ptr<Cell>> m_cells;
 	Vector2d m_grid;
 	Vector2d m_nextGrid;
-
+	std::unordered_set<std::pair<int, int>> m_indexToUpdate;
+	std::unordered_set<std::pair<int, int>> m_nextIndexToUpdate;
 	int m_nbRow{};
 	int m_nbCol{};
 	int m_cellSize{};
